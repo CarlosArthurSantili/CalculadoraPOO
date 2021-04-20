ba1ca200dd1768e288b9a85bcbde140ae885abd3
Carlos Arthur Santili <carlos.a06@aluno.ifsc.edu.br>
2021-04-20 17:01:24 -0300

    Atualizacao1.0
    Exercício da calculadora implementado com programação orientada a objetos

---
+++ CalculadoraPOO/CalculadoraPOO/Calculadora.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPOO
+{
+    class Calculadora
+    {
+        public double n1;
+        public double n2;
+        public string operacao;
+
+        public Calculadora(double n1, double n2, string operacao)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+            this.operacao = operacao;
+        }
+        public double RealizaOperacao()
+        {
+            double resposta = 0.0;
+            switch (operacao)
+            {
+                case "+": resposta = n1 + n2; break;
+                case "-": resposta = n1 - n2; break;
+                case "*": resposta = n1 * n2; break;
+                case "/": resposta = n1 / n2; break;
+                default: break;
+            }
+            return resposta;
+        }
+
+    }
+    
+}
---
+++ CalculadoraPOO/CalculadoraPOO/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Digite o valor do primeiro numero");
+                double n1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Digite o valor do segundo numero");
+                double n2 = Convert.ToDouble(Console.ReadLine());
+            
+                Console.WriteLine("Digite a operação desejada(+, -, /, *)");
+                string operacao = Console.ReadLine();
+
+                if ((operacao != "+") && (operacao != "-") && (operacao != "*") && (operacao != "/"))
+                {
+                    Console.WriteLine("Operação inválida, tente novamente");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else
+                {
+                    Calculadora c = new Calculadora(n1, n2, operacao);
+                    double resposta = c.RealizaOperacao();
+                    Console.WriteLine(resposta);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Número inválido, tente novamente");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
