ec78989c2243e348c20d54b2705adf55a17a3f87
Carlos Arthur Santili <carlos.a06@aluno.ifsc.edu.br>
2021-04-20 17:44:37 -0300

    Atualizacao1.1
    Histórico de operações agora é um atributo da classe Calculadora.

--- CalculadoraPOO/CalculadoraPOO/Calculadora.cs
+++ CalculadoraPOO/CalculadoraPOO/Calculadora.cs
@@ -11,27 +11,51 @@ namespace CalculadoraPOO
         public double n1;
         public double n2;
         public string operacao;
+        public string historico;
 
-        public Calculadora(double n1, double n2, string operacao)
+        public Calculadora(double n1, double n2, string operacao, string historico)
         {
             this.n1 = n1;
             this.n2 = n2;
             this.operacao = operacao;
+            this.historico = historico;
         }
         public double RealizaOperacao()
         {
             double resposta = 0.0;
             switch (operacao)
             {
-                case "+": resposta = n1 + n2; break;
-                case "-": resposta = n1 - n2; break;
-                case "*": resposta = n1 * n2; break;
-                case "/": resposta = n1 / n2; break;
+                case "1": resposta = n1 + n2; break;
+                case "2": resposta = n1 - n2; break;
+                case "3": resposta = n1 * n2; break;
+                case "4": resposta = n1 / n2; break;
                 default: break;
             }
             return resposta;
         }
+        public string AdicionaHistorico(string opcao, double primeiroNumero, double segundoNumero)
+        {
+            string historico;
+            switch (opcao)
+            {
+                case "1":
+                    historico = ($"{primeiroNumero} + {segundoNumero} = {primeiroNumero + segundoNumero} \n");
+                    break;
+                case "2":
+                    historico = ($"{primeiroNumero} - {segundoNumero} = {primeiroNumero - segundoNumero} \n");
+                    break;
+                case "3":
+                    historico = ($"{primeiroNumero} * {segundoNumero} = {primeiroNumero * segundoNumero} \n");
+                    break;
+                case "4":
+                    historico = ($"{primeiroNumero} / {segundoNumero} = {primeiroNumero / segundoNumero} \n");
+                    break;
+                default: //teoricamente, nunca deve cair aqui por verificação anterior a chamada da função
+                    { historico = ""; Console.WriteLine("Opcao Invalida"); break; }
+            }
 
+            return historico;
+        }
     }
     
 }
--- CalculadoraPOO/CalculadoraPOO/Program.cs
+++ CalculadoraPOO/CalculadoraPOO/Program.cs
@@ -1,47 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CalculadoraPOO;
+using System;
 
 namespace CalculadoraPOO
 {
     class Program
     {
+        //1) Somar dois números OK
+        //2) Várias operações de soma OK
+        //3) Várias operações de soma e subtração OK
+        //4) Fazer as 4 operações aritméticas (soma, subtração, multiplicação, divisão) OK
+        //5) Validar a opções do menu OK
+        //B1) BUG: Não permitir divisão por zero OK
+        //6) Mostrar histórico de operações OK
+        //7) Implementar Classe Calculadora OK
         static void Main(string[] args)
         {
-            try
+            double resultado = 0;
+            string opcao = "";
+            string historico = "";
+            do
             {
-                Console.WriteLine("Digite o valor do primeiro numero");
-                double n1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Calculadora 1.7.1");
+                Console.WriteLine("Histórico:");
+                Console.WriteLine($"{historico}");
+                Console.WriteLine("\n");
+                MostrarMenuOpcoes();
+                opcao = Console.ReadLine();
 
-                Console.WriteLine("Digite o valor do segundo numero");
-                double n2 = Convert.ToDouble(Console.ReadLine());
-            
-                Console.WriteLine("Digite a operação desejada(+, -, /, *)");
-                string operacao = Console.ReadLine();
-
-                if ((operacao != "+") && (operacao != "-") && (operacao != "*") && (operacao != "/"))
+                if ((opcao == "s") || (opcao == "S"))
                 {
-                    Console.WriteLine("Operação inválida, tente novamente");
-                    Console.ReadLine();
-                    Console.Clear();
+                    break;
+                }
+                else if ((opcao == "1") || (opcao == "2") || (opcao == "3") || (opcao == "4"))
+                {
+                    try
+                    {
+                        Console.WriteLine("Digite o primeiro número:");
+                        string strPrimeiroNumero = Console.ReadLine();
+                        double primeiroNumero = Convert.ToDouble(strPrimeiroNumero);
+
+                        Console.WriteLine("Digite o segundo número:");
+                        string strSegundoNumero = Console.ReadLine();
+                        double segundoNumero = Convert.ToDouble(strSegundoNumero);
+
+
+                        if ((opcao == "4") && (segundoNumero == 0))
+                        {
+                            Console.WriteLine("Não foi possível realizar a operação divisão, pois não existe divisão por zero");
+                            Console.ReadLine();
+                            Console.Clear();
+                        }
+                        else
+                        {
+                            Calculadora c = new Calculadora(primeiroNumero, segundoNumero, opcao, historico);
+                            resultado = c.RealizaOperacao();
+                            historico += c.AdicionaHistorico(opcao, primeiroNumero, segundoNumero);
+                            ExibirResultado(resultado);
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Erro: Digite um número válido");
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+
                 }
                 else
                 {
-                    Calculadora c = new Calculadora(n1, n2, operacao);
-                    double resposta = c.RealizaOperacao();
-                    Console.WriteLine(resposta);
+                    Console.WriteLine("Opção Inválida");
                     Console.ReadLine();
                     Console.Clear();
                 }
             }
-            catch
-            {
-                Console.WriteLine("Número inválido, tente novamente");
-                Console.ReadLine();
-                Console.Clear();
-            }
+            while (!opcao.Equals("S", StringComparison.OrdinalIgnoreCase));
+
+        }
+
+        private static void MostrarMenuOpcoes()
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("Digite 1 para somar ");
+            Console.WriteLine("Digite 2 para subtrair ");
+            Console.WriteLine("Digite 3 para multiplicar ");
+            Console.WriteLine("Digite 4 para divisão ");
+            Console.WriteLine("Digite S para sair");
+        }
+
+        private static void ExibirResultado(double resultado)
+        {
+            Console.Write("Resultado: ");
+            Console.WriteLine(resultado);
+            Console.ReadLine();
+            Console.Clear();
         }
     }
 }
